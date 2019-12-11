c22230a4ff847ffd8aba6078df5524ec34aa2f59
Kipras Melnikovas <kipras@kipras.org>
2019-12-11 17:29:53 +0200

    Remove broken `extends` entry
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,7 @@ module.exports = {
 	"eslint:recommended",
     "plugin:@typescript-eslint/eslint-recommended",
 	"plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    // "plugin:@typescript-eslint/recommended-requiring-type-checking", /** broken atm */
     "prettier",
     "prettier/react",
     "prettier/@typescript-eslint",
