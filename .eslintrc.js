14ebedb9b60b3437cf9c42839ac243522e2faf04
Kipras Melnikovas <kipras@kipras.org>
2020-04-27 21:53:33 +0300

    Disable `react/prop-types`
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- .eslintrc.js
+++ .eslintrc.js
@@ -110,6 +110,7 @@ module.exports = {
 		"react/react-in-jsx-scope": 0,
 		"react/prefer-stateless-function": 0,
 		"react/forbid-prop-types": 0,
+		"react/prop-types": 0,
 		"react/no-unescaped-entities": 0,
 		"jsx-a11y/accessible-emoji": 0,
 		"react/require-default-props": 0,
