2e05a892c7dae984f3a0bd5081ebeeff350a12f0
mhesham32 <m.hesham3001@gmail.com>
2020-10-09 19:15:00 +0200

    add "s" to window (fix typo) 🤞

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
-  extends: ['airbnb', 'prettier', 'prettier/react'],
-  parser: 'babel-eslint',
+  extends: ["airbnb", "prettier", "prettier/react"],
+  parser: "babel-eslint",
   parserOptions: {
     ecmaVersion: 2020,
     // Can I remove these now?
@@ -16,102 +16,102 @@ module.exports = {
     jest: true,
   },
   rules: {
-    'no-debugger': 0,
-    'no-alert': 0,
-    'no-await-in-loop': 0,
-    'no-return-assign': ['error', 'except-parens'],
-    'no-restricted-syntax': [
+    "no-debugger": 0,
+    "no-alert": 0,
+    "no-await-in-loop": 0,
+    "no-return-assign": ["error", "except-parens"],
+    "no-restricted-syntax": [
       2,
-      'ForInStatement',
-      'LabeledStatement',
-      'WithStatement',
+      "ForInStatement",
+      "LabeledStatement",
+      "WithStatement",
     ],
-    'no-unused-vars': [
+    "no-unused-vars": [
       1,
       {
         ignoreRestSiblings: true,
-        argsIgnorePattern: 'res|next|^err',
+        argsIgnorePattern: "res|next|^err",
       },
     ],
-    'prefer-const': [
-      'error',
+    "prefer-const": [
+      "error",
       {
-        destructuring: 'all',
+        destructuring: "all",
       },
     ],
-    'arrow-body-style': [2, 'as-needed'],
-    'no-unused-expressions': [
+    "arrow-body-style": [2, "as-needed"],
+    "no-unused-expressions": [
       2,
       {
         allowTaggedTemplates: true,
       },
     ],
-    'no-param-reassign': [
+    "no-param-reassign": [
       2,
       {
         props: false,
       },
     ],
-    'no-console': 0,
-    'import/prefer-default-export': 0,
+    "no-console": 0,
+    "import/prefer-default-export": 0,
     import: 0,
-    'func-names': 0,
-    'space-before-function-paren': 0,
-    'comma-dangle': 0,
-    'max-len': 0,
-    'import/extensions': 0,
-    'no-underscore-dangle': 0,
-    'consistent-return': 0,
-    'react/display-name': 1,
-    'react/no-array-index-key': 0,
-    'react/react-in-jsx-scope': 0,
-    'react/prefer-stateless-function': 0,
-    'react/forbid-prop-types': 0,
-    'react/no-unescaped-entities': 0,
-    'jsx-a11y/accessible-emoji': 0,
-    'react/require-default-props': 0,
-    'react/jsx-filename-extension': [
+    "func-names": 0,
+    "space-before-function-paren": 0,
+    "comma-dangle": 0,
+    "max-len": 0,
+    "import/extensions": 0,
+    "no-underscore-dangle": 0,
+    "consistent-return": 0,
+    "react/display-name": 1,
+    "react/no-array-index-key": 0,
+    "react/react-in-jsx-scope": 0,
+    "react/prefer-stateless-function": 0,
+    "react/forbid-prop-types": 0,
+    "react/no-unescaped-entities": 0,
+    "jsx-a11y/accessible-emoji": 0,
+    "react/require-default-props": 0,
+    "react/jsx-filename-extension": [
       1,
       {
-        extensions: ['.js', '.jsx'],
+        extensions: [".js", ".jsx"],
       },
     ],
     radix: 0,
-    'no-shadow': [
+    "no-shadow": [
       2,
       {
-        hoist: 'all',
-        allow: ['resolve', 'reject', 'done', 'next', 'err', 'error'],
+        hoist: "all",
+        allow: ["resolve", "reject", "done", "next", "err", "error"],
       },
     ],
     quotes: [
       2,
-      'single',
+      "single",
       {
         avoidEscape: true,
         allowTemplateLiterals: true,
       },
     ],
-    'prettier/prettier': [
-      'error',
+    "prettier/prettier": [
+      "error",
       {
-        trailingComma: 'es5',
+        trailingComma: "es5",
         singleQuote: true,
         printWidth: 80,
-        //below line only for window users facing CLRF and eslint/prettier error
-        // non window users feel free to delete it
-        endOfLine: 'auto'
+        //below line only for windows users facing CLRF and eslint/prettier error
+        // non windows users feel free to delete it
+        endOfLine: "auto",
       },
     ],
-    'jsx-a11y/href-no-hash': 'off',
-    'jsx-a11y/anchor-is-valid': [
-      'warn',
+    "jsx-a11y/href-no-hash": "off",
+    "jsx-a11y/anchor-is-valid": [
+      "warn",
       {
-        aspects: ['invalidHref'],
+        aspects: ["invalidHref"],
       },
     ],
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
   },
-  plugins: ['html', 'prettier', 'react-hooks'],
+  plugins: ["html", "prettier", "react-hooks"],
 };
