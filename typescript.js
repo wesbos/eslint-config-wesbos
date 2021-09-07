f00217da315918847bf600f1970e3e4a29fde071
Wes Bos <wesbos@gmail.com>
2021-09-07 12:05:11 -0400

    remove prettier TS specific as its all bundled into one now

--- typescript.js
+++ typescript.js
@@ -1,7 +1,6 @@
 module.exports = {
   extends: [
     'plugin:@typescript-eslint/recommended', // Uses rules from `@typescript-eslint/eslint-plugin`,
-    'prettier/@typescript-eslint', // Use `eslint-config-prettier` to override conflicting rules from `@typescript-eslint/eslint-plugin`
     'airbnb-typescript',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
