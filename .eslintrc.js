29dd4482bdfd447ba3c8251ca808a8d465bfe233
Wes Bos <wesbos@gmail.com>
2022-05-13 14:27:52 -0400

    turn off no use before define

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
   },
   rules: {
     'no-debugger': 0,
+    'no-use-before-define': "off",
     'no-alert': 0,
     'no-await-in-loop': 0,
     'no-return-assign': ['error', 'except-parens'],
